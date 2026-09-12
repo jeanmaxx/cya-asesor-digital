e071f9c401f7f47b333d68549cde04fee1102549
jeanmaxx <69431920+jeanmaxx@users.noreply.github.com>
2026-09-12 01:37:53 -0600

    Agregar configuración reutilizable del asesor

---
+++ assets/config.js
@@ -0,0 +1,12 @@
+window.ADVISOR_CONFIG = {
+  name: "José Emmanuel Álvarez Nieto",
+  role: "Asesor Previsional",
+  phoneDisplay: "442 286 7965",
+  phoneE164: "+524422867965",
+  whatsappE164: "524422867965",
+  whatsappDefaultMessage: "Hola Emmanuel, vi tu tarjeta digital y me gustaría recibir asesoría previsional.",
+  instagramUrl: "",
+  facebookUrl: "",
+  photoUrl: "assets/profile-placeholder.svg",
+  logoUrl: "assets/logo-placeholder.svg"
+};
